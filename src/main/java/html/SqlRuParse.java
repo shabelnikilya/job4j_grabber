9b9594c6da4b90fb43b188b24574060c3101d14d
shabelnikilya <shabelnik_ilya@mail.ru>
2021-11-03 21:57:21 +0300

    2. Парсинг HTML страницы. [#260358 #224014]

--- src/main/java/html/SqlRuParse.java
+++ src/main/java/html/SqlRuParse.java
@@ -8,10 +8,13 @@ import org.jsoup.select.Elements;
 public class SqlRuParse {
     public static void main(String[] args) throws Exception {
         Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
-        Elements row = doc.select(".icon_cell");
+        Elements row = doc.select(".postslisttopic");
         for (Element td : row) {
             Element par = td.parent();
-            System.out.println(par.children().get(5).text());
+            Element href = td.child(0);
+            System.out.println("Название вакансии - " + href.text());
+            System.out.println("Ссылка на вакансию - " + href.attr("href"));
+            System.out.println("Дата опубликования вакансии - " + par.children().get(5).text());
         }
     }
 }
