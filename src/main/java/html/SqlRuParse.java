40ee59e74eec6400e3c9442a7e6ff38535d3697b
shabelnikilya <shabelnik_ilya@mail.ru>
2021-11-03 22:27:30 +0300

    2.1.1. Парсинг https://www.sql.ru/forum/job-offers/3 [#285210]

--- src/main/java/html/SqlRuParse.java
+++ src/main/java/html/SqlRuParse.java
@@ -7,14 +7,16 @@ import org.jsoup.select.Elements;
 
 public class SqlRuParse {
     public static void main(String[] args) throws Exception {
-        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
-        Elements row = doc.select(".postslisttopic");
-        for (Element td : row) {
-            Element par = td.parent();
-            Element href = td.child(0);
-            System.out.println("Название вакансии - " + href.text());
-            System.out.println("Ссылка на вакансию - " + href.attr("href"));
-            System.out.println("Дата опубликования вакансии - " + par.children().get(5).text());
+        for (int i = 1; i < 6; i++) {
+            Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers/" + i).get();
+            Elements row = doc.select(".postslisttopic");
+            for (Element td : row) {
+                Element par = td.parent();
+                Element href = td.child(0);
+                System.out.println("Название вакансии - " + href.text());
+                System.out.println("Ссылка на вакансию - " + href.attr("href"));
+                System.out.println("Дата опубликования вакансии - " + par.children().get(5).text());
+            }
         }
     }
 }
