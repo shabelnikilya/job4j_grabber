eb3242bf53cf9668f7245b2652be0c0c1b919662
shabelnikilya <shabelnik_ilya@mail.ru>
2021-11-03 21:40:46 +0300

    1.1. Job c параметрами [#260360]

--- src/main/java/quartz/AlertRabbit.java
+++ src/main/java/quartz/AlertRabbit.java
@@ -6,8 +6,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import java.io.IOException;
 import java.io.InputStream;
+import java.sql.*;
 import java.util.Properties;
-
 import static org.quartz.JobBuilder.*;
 import static org.quartz.TriggerBuilder.*;
 import static org.quartz.SimpleScheduleBuilder.*;
@@ -16,42 +16,72 @@ public class AlertRabbit {
 
     private static final Logger LOG = LoggerFactory.getLogger(AlertRabbit.class.getName());
 
-    public static int getProperties(String key) {
-        int time = -1;
+    public static String getProperties(String key) {
         Properties config = new Properties();
         try (InputStream in = AlertRabbit
                 .class.getClassLoader()
                 .getResourceAsStream("rabbit.properties")) {
             config.load(in);
-            time = Integer.parseInt(config.getProperty(key));
-        } catch (IOException | NumberFormatException e) {
-            LOG.error("I/O or NumberFormatException(Check the values in rabbit.properties) exception", e);
+        } catch (IOException io) {
+            LOG.error("I/O exception", io);
         }
-        return time;
+        return config.getProperty(key);
+    }
+
+    public static int ifValidGetTime(String key) {
+        int value = -1;
+        try {
+            value = Integer.parseInt(getProperties(key));
+        } catch (NumberFormatException nfe) {
+            LOG.error("Check the values in rabbit.properties, by key - rabbit.interval");
+        }
+        return value;
+    }
+
+    public static Connection initConnection() throws ClassNotFoundException, SQLException {
+        Class.forName(getProperties("jdbc.driver"));
+        return DriverManager.getConnection(getProperties("jdbc.url"),
+                getProperties("jdbc.username"),
+                getProperties("jdbc.password"));
     }
 
     public static void main(String[] args) {
-        try {
+        try (Connection connection = initConnection()) {
             Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
             scheduler.start();
-            JobDetail job = newJob(Rabbit.class).build();
+            JobDataMap data = new JobDataMap();
+            data.put("connection", connection);
+            JobDetail job = newJob(Rabbit.class)
+                    .usingJobData(data)
+                    .build();
             SimpleScheduleBuilder times = simpleSchedule()
-                    .withIntervalInSeconds(getProperties("rabbit.interval"))
+                    .withIntervalInSeconds(ifValidGetTime("rabbit.interval"))
                     .repeatForever();
             Trigger trigger = newTrigger()
                     .startNow()
                     .withSchedule(times)
                     .build();
             scheduler.scheduleJob(job, trigger);
-        } catch (SchedulerException se) {
-            LOG.error("Stack exception: ", se);
+            Thread.sleep(ifValidGetTime("main.time"));
+            scheduler.shutdown();
+        } catch (SchedulerException | InterruptedException
+                | SQLException | ClassNotFoundException e) {
+            LOG.error("Stack exception: ", e);
         }
     }
 
     public static class Rabbit implements Job {
+
         @Override
-        public void execute(JobExecutionContext jobExecutionContext) {
+        public void execute(JobExecutionContext context) {
             System.out.println("Rabbit runs here ...");
+            Connection connection = (Connection) context.getJobDetail().getJobDataMap().get("connection");
+            try (PreparedStatement st = connection.prepareStatement("insert into rabbit(created_date) values (?);")) {
+                st.setTimestamp(1,  new Timestamp(System.currentTimeMillis()));
+                st.execute();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 }
