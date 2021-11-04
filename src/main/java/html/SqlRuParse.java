4046b798aa746322f2ed48bcb42059b9d370dd6e
shabelnikilya <shabelnik_ilya@mail.ru>
2021-11-04 22:52:23 +0300

    2.3. Загрузка деталей поста. [#285212]

--- src/main/java/html/SqlRuParse.java
+++ src/main/java/html/SqlRuParse.java
@@ -4,6 +4,7 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import java.util.Objects;
 
 public class SqlRuParse {
     public static void main(String[] args) throws Exception {
@@ -13,11 +14,32 @@ public class SqlRuParse {
             for (Element td : row) {
                 Element par = td.parent();
                 Element href = td.child(0);
+                Document docPost = Jsoup.connect(href.attr("href")).get();
                 System.out.println("Название вакансии - " + href.text());
                 System.out.println("Ссылка на вакансию - " + href.attr("href"));
                 System.out.println("Дата опубликования вакансии - " + par.children().get(5).text());
+                System.out.println(getDescription(docPost));
+                System.out.println(getDateCreatedPost(docPost));
             }
         }
+    }
+
+    public static String getDescription(Document link) {
+        Elements row = link.select(".msgBody");
+        Element parent = row.stream().skip(1).findFirst().orElse(null);
+        return Objects.requireNonNull(parent).text();
+    }
+
+    public static String getDateCreatedPost(Document link) {
+        Elements w = link.select(".msgFooter");
+        Element p = w.stream().findFirst().orElse(null);
+        return splitString(Objects.requireNonNull(p).text());
+    }
+
+    public static String splitString(String date) {
+        StringBuilder str = new StringBuilder(date);
+        int index = str.indexOf(":");
+        return str.substring(0, index + 3);
     }
 }
 
