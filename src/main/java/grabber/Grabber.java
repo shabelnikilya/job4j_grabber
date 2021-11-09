eafb320774c2b046ecf1e69e14af895bfbf22511
shabelnikilya <shabelnik_ilya@mail.ru>
2021-11-09 16:13:21 +0300

    7. Web. [#289478]

--- src/main/java/grabber/Grabber.java
+++ src/main/java/grabber/Grabber.java
@@ -7,6 +7,10 @@ import utils.DateTimeParser;
 import utils.SqlRuDateTimeParser;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.Charset;
 import java.util.*;
 import static org.quartz.JobBuilder.newJob;
 import static org.quartz.SimpleScheduleBuilder.simpleSchedule;
@@ -84,6 +88,28 @@ public class Grabber implements Grab {
             Store store = grab.store();
             DateTimeParser dateTimeParser = new SqlRuDateTimeParser();
             grab.init(new SqlRuParse(dateTimeParser), store, scheduler);
+            grab.web(store);
         }
+    }
+
+    private void web(Store store) {
+        new Thread(() -> {
+            try (ServerSocket server = new ServerSocket(Integer.parseInt(cfg.getProperty("port")))) {
+                while (!server.isClosed()) {
+                    Socket socket = server.accept();
+                    try (OutputStream out = socket.getOutputStream()) {
+                        out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                        for (Post post : store.getAll()) {
+                            out.write(post.toString().getBytes(Charset.forName("Windows-1251")));
+                            out.write(System.lineSeparator().getBytes());
+                        }
+                    } catch (IOException io) {
+                        io.printStackTrace();
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }).start();
     }
 }
